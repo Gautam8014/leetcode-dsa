bd313af562fbaf6e019158e0921d937e0dcab6db
Gautam Singh <gautam.singh@vml.com>
2026-01-12 18:31:58 +0530

    array

---
+++ Arrays/TwoSum.java
@@ -0,0 +1,11 @@
+public class TwoSum {
+    public static void main(String[] args) {
+        // Example input
+        int[] arr = {2, 7, 11, 15};
+        int target = 9;
+
+        // Logic goes here
+        // Example output
+        System.out.println(5 + 2);
+    }
+}
