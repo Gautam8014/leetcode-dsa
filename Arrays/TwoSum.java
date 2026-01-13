bbc952382868db5309c977ab0e5b952a26f2acd8
Gautam Singh <gautam.singh@vml.com>
2026-01-13 20:24:51 +0530

    find missing number

--- Arrays/TwoSum.java
+++ Arrays/TwoSum.java
@@ -1,11 +1,38 @@
 public class TwoSum {
     public static void main(String[] args) {
-        // Example input
-        int[] arr = {2, 7, 11, 15};
-        int target = 9;
+        // check if an array is sorted  ?
 
-        // Logic goes here
-        // Example output
-        System.out.println(5 + 2);
+
+        int[] arr = {1, 2, 3, 4, 5, 7, 8, 9, 10};
+       
+          int num=0;
+
+
+        for(int i=0;i<arr.length;i++){
+        
+       if(arr[i]  != i+1){
+              System.out.println(i+1);
+             break;
+       }else{
+        num++;
+       }
+
+
+    }
+
+    if(num==arr.length){
+ System.out.println("all number is present");
+
+    }
+
+   
+         
+             
+        
+
+
+
+
+      
     }
 }
