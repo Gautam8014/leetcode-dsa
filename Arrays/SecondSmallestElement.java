3680fa2c68b6c3edf801be290688c983d278927c
Gautam Singh <gautam.singh@vml.com>
2026-01-19 20:21:40 +0530

    DuplicateCharacters

--- Arrays/SecondSmallestElement.java
+++ Arrays/SecondSmallestElement.java
@@ -2,6 +2,22 @@
 public class SecondSmallestElement {
 
     public static void main(String[] args) {
-        System.out.println("SecondSmallestElement program running");
+      int[] arr = new int[]{1, 2, 3, 4, 5, 7, 8, 6, 15};
+
+       int small =99999;
+                int ssmall= 9999;
+         for(int i=0;i<arr.length;i++){
+    if(arr[i]<small){
+        small =arr[i];
+    }
+         }
+
+         for(int i=0;i<arr.length;i++){
+            if(arr[i]<ssmall && arr[i]!=small){
+                ssmall=arr[i];
+            }
+         }
+
+         System.out.println(ssmall);
     }
 }
--- Strings/DuplicateCharacters.java
+++ Strings/DuplicateCharacters.java
@@ -2,6 +2,19 @@
 public class DuplicateCharacters {
 
     public static void main(String[] args) {
-        System.out.println(""DuplicateCharacters program running"");
+        String str = "programming";
+ System.out.println("Duplicate characters:");
+        for(int i=0;i<str.length();i++){
+            int count=1;
+
+            for(int j=i+1;j<str.length();j++){
+                if(str.charAt(i) == str.charAt(j)){
+                    count++;
+                }
+            }
+            if(count>1){
+                System.out.println(str.charAt(i));
+            }
+        }
     }
 }
