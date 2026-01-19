5ce7429e3c8f2ea660f19c15e795f43e3b2db1cc
Gautam Singh <gautam.singh@vml.com>
2026-01-19 16:19:38 +0530

    length

--- Strings/StringLength.java
+++ Strings/StringLength.java
@@ -2,6 +2,12 @@
 public class StringLength {
 
     public static void main(String[] args) {
-        System.out.println(""StringLength program running"");
+        String str = "mylaptop";
+          int count =0;
+        for(int i=0;i<str.length();i++){
+             count++;
+        }
+
+        System.out.println(count);
     }
 }
